358235ef05b4722fbc65f1cb38516dbf6055b2e0
Dominik Mueller <dominikmueller1989@googlemail.com>
2016-12-15 10:53:37 +0000

    Properly added variables to the code base and provide easy access and buffer functionality.

--- analysis/add_variables.py
+++ analysis/add_variables.py
@@ -2,7 +2,8 @@ from k3pi_utilities import variables as vars
 from k3pi_utilities.selective_load import selective_load
 from k3pi_utilities.buffer import buffer_load
 from k3pi_utilities.debugging import call_debug
-from k3pi_cpp import compute_delta_angle
+from k3pi_config.modes import gcm
+from k3pi_cpp import compute_delta_angle, vec_phsp_variables
 from k3pi_config import config
 import pandas as pd
 
@@ -25,3 +26,27 @@ def _dstp_slowpi_angle(df, mode):
         config.PDG_MASSES[config.pion],
     )
     return pd.Series(ret, name='dstp_slowpi_angle')
+
+
+@buffer_load
+@selective_load
+@call_debug
+def phsp_variables(df):
+    """Returns m12, m34, cos1, cos2, phi1"""
+    mode = gcm()
+
+    vals = vec_phsp_variables(
+        df[vars.dtf_pt(mode.K)], df[vars.dtf_eta(mode.K)],
+        df[vars.dtf_phi(mode.K)], config.PDG_MASSES['K'],
+        df[vars.dtf_pt(mode.Pi_OS1)], df[vars.dtf_eta(mode.Pi_OS1)],
+        df[vars.dtf_phi(mode.Pi_OS1)], config.PDG_MASSES['Pi'],
+        df[vars.dtf_pt(mode.Pi_SS)], df[vars.dtf_eta(mode.Pi_SS)],
+        df[vars.dtf_phi(mode.Pi_SS)], config.PDG_MASSES['Pi'],
+        df[vars.dtf_pt(mode.Pi_OS2)], df[vars.dtf_eta(mode.Pi_OS2)],
+        df[vars.dtf_phi(mode.Pi_OS2)], config.PDG_MASSES['Pi'])
+
+    return pd.DataFrame({'m12': vals[0],
+                         'm34': vals[1],
+                         'cos1': vals[2],
+                         'cos2': vals[3],
+                         'phi1': vals[5]})
--- k3pi_utilities/selective_load.py
+++ k3pi_utilities/selective_load.py
@@ -1,6 +1,8 @@
 from collections import defaultdict
 from k3pi_config import config
 from k3pi_config import modes
+from k3pi_config.modes import gcm
+import inspect
 
 accumulated_per_mode = defaultdict(lambda: set())
 
@@ -16,13 +18,19 @@ class selective_load():
             function(d, mode_cls)
             self.requested_columns[m] = d.keys()
             [accumulated_per_mode[m].add(n) for n in d.keys()]
+        self._wants_mode = 'mode' in inspect.getargspec(function).args
         self._func = function
         self._func_name = function.__name__
         self.__name__ = function.__name__
         self.__doc__ = function.__doc__
 
-    def __call__(self, mode):
+    def __call__(self, mode=None):
+        if mode is None:
+            mode = gcm()
         df = mode.get_data(self.requested_columns[mode.mode])
-        ret = self._func(df, mode)
+        if self._wants_mode:
+            ret = self._func(df, mode)
+        else:
+            ret = self._func(df)
         del df
         return ret
